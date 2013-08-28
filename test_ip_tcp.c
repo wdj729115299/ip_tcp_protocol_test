aad16c11bc630e0d62ccc49ccde50a4f3d0324dc
zly <dear531@21cn.com>
2013-08-28 18:19:59 +0800

    addtion tcp headel argument show

--- test_ip_tcp.c
+++ test_ip_tcp.c
@@ -37,11 +37,12 @@ unsigned int kook_func(unsigned int hooknum,
 #define IP_DF		0x4000		/* Flag: "Don't Fragment"	*/
 #define IP_MF		0x2000		/* Flag: "More Fragments"	*/
 #define IP_OFFSET	0x1FFF		/* "Fragment Offset" part	*/
-#endif
 	pr_info("IP_OFFSET:%04x\n", IP_OFFSET);
 	pr_info("ip_addr->frag_off IP_DF:%d\n",ip_addr->frag_off & IP_DF);
 	pr_info("ip_addr->frag_off IP_MF:%d\n",ip_addr->frag_off & IP_MF);
+#endif
 	
+#if 1
 #if 1
 	pr_info("version:%d, ihl:%d, tos:%x, tot_len:%d,id:%d, frag_off:%d,\
 ttl:%d, protocol:%d, check:%d, saddr:%pI4, daddr:%pI4\n",\
@@ -56,7 +57,28 @@ ttl:%d, protocol:%d, check:%d, saddr:%pI4, daddr:%pI4\n",\
 	ip_addr->check,
 	&ip_addr->saddr,
 	&ip_addr->daddr);
+#endif
 
+	pr_info("source:%d, dest:%d, seq:%d, ack_seq:%d, res1:%d,\
+doff:%d, fin:%d, syn:%d, rst:%d, psh:%d, ack:%d, urg:%d, ece:%d,\
+cwr:%d, window:%d, check:%d, urg_ptr:%d\n",\
+	ntohs(tcp_addr->source),
+	ntohs(tcp_addr->dest),
+	ntohs(tcp_addr->seq),
+	ntohs(tcp_addr->ack_seq),
+	tcp_addr->res1,
+	tcp_addr->doff,
+	tcp_addr->fin,
+	tcp_addr->syn,
+	tcp_addr->rst,
+	tcp_addr->psh,
+	tcp_addr->ack,
+	tcp_addr->urg,
+	tcp_addr->ece,
+	tcp_addr->cwr,
+	ntohs(tcp_addr->window),
+	tcp_addr->check,
+	ntohs(tcp_addr->urg_ptr));
 #else
 	pr_info("ip_addr->version:%d", ip_addr->version);
 	pr_info("ip_addr->ihl:%d", ip_addr->ihl);
